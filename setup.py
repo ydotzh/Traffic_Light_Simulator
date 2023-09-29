f71ea92d0a7ba0679790a9bbc7b6fc25b118ee71
ydotzh <146397338+ydotzh@users.noreply.github.com>
2023-09-29 10:08:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'traffic_light_simulator_demo',
   packages = ['traffic_light_simulator_demo'],
   include_package_data=True,
-  version = '0.12',
+  version = '0.13',
   license='MIT',
   description = 'traffic light simulator', 
   author = 'Yan', 
