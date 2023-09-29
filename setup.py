c517fd04e72c66f2738ba38dd9efd13a667f066d
ydotzh <146397338+ydotzh@users.noreply.github.com>
2023-09-29 06:53:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'traffic_light_simulator_demo',
   packages = ['traffic_light_simulator_demo'],
-  version = '0.8',
+  version = '0.9',
   license='MIT',
   description = 'traffic light simulator', 
   author = 'Yan', 
