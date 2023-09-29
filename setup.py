3dc1eba4e24efeb04d199e4e8931ae0bdb16071e
ydotzh <146397338+ydotzh@users.noreply.github.com>
2023-09-29 06:08:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
   name = 'traffic_light_simulator_demo',
   packages = find_packages(),
-  version = '0.3',
+  version = '0.4',
   license='MIT',
   description = 'traffic light simulator', 
   author = 'Yan', 
@@ -13,12 +13,5 @@ setup(
   download_url = 'https://github.com/ydotzh/Traffic_Light_Simulator/archive/refs/tags/v_02.tar.gz',
   keywords = ['Data Challenge'],
   classifiers=[
-    'Development Status :: 3 - Alpha', 
-    'Intended Audience :: Developers', 
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3', 
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
   ],
 )
